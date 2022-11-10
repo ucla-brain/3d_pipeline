73470ee07a69b632a496a0e65aece99300fd64f2
lhgarciaa <lhgarciaa@g.ucla.edu>
2022-11-10 14:49:49 -0800

    Add downsample and registration slurm sbatch files

---
+++ slurm_3d_downsample.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+#SBATCH --job-name=3d_downsample
+#SBATCH --output=out_%j_%u.log
+#SBATCH --nodes=1
+#SBATCH --mem=32G
+
+export E_BADARGS=65
+
+# Check for correct number of arguments
+if [ ! -n "$4" ]
+then
+    echo "Usage: `basename $0` <input-path> <channel> <file-type> <resolution>"
+    exit $E_BADARGS
+fi
+
+module load singularity
+
+# Set singularity alias for compact command later
+shopt -s expand_aliases
+alias 3dDownsample='singularity exec -B /panfs 3d_downsampling.sif /code/3d_downsample.py'
+
+file=$1
+channel=$2
+type=$3
+resolution=$4
+
+# Start downsample
+date
+echo "Running '3dDownsample $file -c $channel -t $type -res $resolution'"
+3dDownsample $file -c $channel -t $type -res $resolution
+echo "done..."
---
+++ slurm_3d_registration.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+#SBATCH --job-name=3d_registration
+#SBATCH --output=out_%j_%u.log
+#SBATCH --nodes=1
+#SBATCH --mem=32G
+
+export E_BADARGS=65
+
+# Check for correct number of arguments
+if [ ! -n "$5" ]
+then
+    echo "Usage: `basename $0` <target-name> <output-dir> <output-file> <config-file> <atlas>"
+    exit $E_BADARGS
+fi
+
+module load singularity
+
+# Set singularity alias for compact command later
+shopt -s expand_aliases
+alias 3dRegistration='singularity exec -B /panfs 3d_registration.sif /code/3d_registration.py'
+
+target_name$1
+output_dir=$2
+output_file=$3
+config_file=$4
+atlas=$5
+
+# Start registration
+date
+echo "Running '3dRegistration $file -c $channel -t $type -res $resolution'"
+3dRegistration $target_name -c -od $output_dir -of $output_file -cf $config_file -a $atlas
+echo "done..."
