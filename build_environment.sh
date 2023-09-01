8c7424a93a2dd1d0750e07630b103969f79ff4f6
lhgarciaa <lhgarciaa@g.ucla.edu>
2023-09-01 14:33:15 -0700

    Drop 'set e' in build script to prevent terminal closing

--- build_environment.sh
+++ build_environment.sh
@@ -5,14 +5,20 @@
 
 ENV_NAME="3d_pipeline"
 
+# checks if previous command finished successfully
+if_error_echo() {
+    if [ $? -ne 0 ]; then
+        # echo message if provided
+        echo $1
+        exit 61
+    fi
+}
+
 # Check if conda is installed
 if ! command -v conda &>/dev/null; then
     echo "Anaconda (or Miniconda) is not installed. Please install or load it before running this script."
     exit 1
 fi
-
-# Activate error handling
-set -e
 
 # Check if the environment already exists
 if conda info --envs | grep -q "$ENV_NAME"; then
@@ -21,9 +27,10 @@ if conda info --envs | grep -q "$ENV_NAME"; then
 else
     echo "Creating and activating a new Anaconda environment: $ENV_NAME"
     conda create -y -n "$ENV_NAME" python=3.9
+    if_error_echo "Problem creating Anaconda environment"
     conda activate "$ENV_NAME"
+    if_error_echo "Could not activate environment, this file must be sourced"
     pip install -r requirements.txt
+    if_error_echo "Problem installing pip requirements"
     echo "Environment ready"
 fi
-
-set +e
